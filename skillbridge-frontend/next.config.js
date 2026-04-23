ab5061cd38c24e67deddaf8db495b5a42ea4552b
Humer076 <EMAIL_LINKED_TO_HUMER076>
2026-04-23 11:27:34 +0530

    fix: remove env block from next.config.js

--- skillbridge-frontend/next.config.js
+++ skillbridge-frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-  },
 };
 
 module.exports = nextConfig;
